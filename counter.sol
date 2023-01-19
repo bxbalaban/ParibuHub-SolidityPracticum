61135362d045d293bcbb077554e5d27c70db45ff
bxbalaban <balabanbsr@gmail.com>
2023-01-20 02:47:45 +0300

    comments added

--- counter.sol
+++ counter.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.7;
 
 contract Counter{
+    //Global variables for holding the values
     uint public count;
 
+    //function to increase count value by 1
     function inc() external{
         count+=1;
     }
+
+    //function to decrease count value by 1
     function dec() external{
         count-=1;
     }
